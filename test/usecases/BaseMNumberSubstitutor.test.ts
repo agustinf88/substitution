8e1952c3b52fa8ddc39d53bcd0e740da73be07a6
Agustin <agustinf88@gmail.com>
2020-11-06 13:19:49 -0300

    add border case

--- test/usecases/BaseMNumberSubstitutor.test.ts
+++ test/usecases/BaseMNumberSubstitutor.test.ts
@@ -41,3 +41,31 @@ test("D = 5 and e = 5 should return 7.5", () => {
     expect(actual).toEqual(7.5);
 
 });
+
+test("D = 2.5 and e = 2 should return 3", () => {
+    const useCase = new BaseMNumberSubstitutor();
+
+    const numberValues = {
+        d: 2.5,
+        e: 2,
+    };
+
+    const actual = useCase.apply(numberValues);
+
+    expect(actual).toEqual(3);
+
+});
+
+test("D = 2.3 and e = 2 should return 2.76", () => {
+    const useCase = new BaseMNumberSubstitutor();
+
+    const numberValues = {
+        d: 2.3,
+        e: 2,
+    };
+
+    const actual = useCase.apply(numberValues);
+
+    expect(actual).toEqual(2.76);
+
+});
