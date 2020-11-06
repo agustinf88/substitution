aed7f703632daf2e8a93fc16661a9dbe22c2946a
Agustin <agustinf88@gmail.com>
2020-11-06 15:24:16 -0300

    add P case

--- src/usecases/number/BaseNumberSubstitutorFactory.ts
+++ src/usecases/number/BaseNumberSubstitutorFactory.ts
@@ -1,10 +1,13 @@
 import { NumberSubstitutor, NumberSubstitutorFactory } from "../interfaces";
 import { BaseMNumberSubstitutor } from "./BaseMNumberSubstitutor";
+import { BasePNumberSubstitutor } from "./BasePNumberSubstitutor";
 
 export class BaseNumberSubstitutorFactory implements NumberSubstitutorFactory {
     create(value: string): NumberSubstitutor {
         if (value === "M") {
             return new BaseMNumberSubstitutor();
+        } else if (value === "P"){
+            return new BasePNumberSubstitutor();
         }
         throw new Error("Method not implemented.");
     }
--- test/usecases/number/BaseNumberSubstitutorFactory.test.ts
+++ test/usecases/number/BaseNumberSubstitutorFactory.test.ts
@@ -1,14 +1,22 @@
 import { BaseNumberSubstitutorFactory } from "../../../src/usecases/number/BaseNumberSubstitutorFactory"
 import {BaseMNumberSubstitutor} from "../../../src/usecases/number/BaseMNumberSubstitutor"
+import {BasePNumberSubstitutor} from "../../../src/usecases/number/BasePNumberSubstitutor"
 
 test("When unknown value the throws with description", () => {
     const useCase = new BaseNumberSubstitutorFactory();
     expect(() => useCase.create("A")).toThrowError(new Error("Method not implemented."));
 });
 
-test("When M should return instance of BaseNumberSubstitutor", () => {
+test("When M should return instance of BaseMNumberSubstitutor", () => {
     const useCase = new BaseNumberSubstitutorFactory();
     const numberSubstitutor = useCase.create("M");
 
     expect(numberSubstitutor).toBeInstanceOf(BaseMNumberSubstitutor)
+});
+
+test("When P should return instance of BasePNumberSubstitutor", () => {
+    const useCase = new BaseNumberSubstitutorFactory();
+    const numberSubstitutor = useCase.create("P");
+
+    expect(numberSubstitutor).toBeInstanceOf(BasePNumberSubstitutor)
 });
