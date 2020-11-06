96acc710b1baab5310c7531c6e25efc0af1a7ab6
Agustin <agustinf88@gmail.com>
2020-11-06 14:49:22 -0300

    add test

---
+++ test/usecases/CustomTwoBoolSubstitutor.test.ts
@@ -0,0 +1,61 @@
+import { CustomTwoBoolSubstitutor } from "../../src/usecases/CustomTwoBoolSubstitutor"
+
+test('when A && B && !C then T', () => {
+    const useCase = new CustomTwoBoolSubstitutor();
+
+    const boolValues = {
+        a: true,
+        b: true,
+        c: false
+    };
+
+    const actual = useCase.apply(boolValues);
+
+    expect(actual).toEqual("T");
+
+});
+
+test('when A && B && C then throws with message Combination not implemented', () => {
+    const useCase = new CustomTwoBoolSubstitutor();
+
+    const boolValues = {
+        a: true,
+        b: true,
+        c: true
+    };
+
+    expect(() => useCase.apply(boolValues)) 
+        .toThrowError(new Error("Combination not implemented"));
+
+});
+
+test('A && !B && C then M', () => {
+    const useCase = new CustomTwoBoolSubstitutor();
+
+    const boolValues = {
+        a: true,
+        b: false,
+        c: true
+    };
+
+    const actual = useCase.apply(boolValues);
+
+    expect(actual).toEqual("M");
+
+});
+
+test('when !A && !B && !C then throws with message Combination not implemented', () => {
+    const useCase = new CustomTwoBoolSubstitutor();
+
+    const boolValues = {
+        a: false,
+        b: false,
+        c: false
+    };
+
+    expect(() => useCase.apply(boolValues)) 
+        .toThrowError(new Error("Combination not implemented"));
+
+});
+
+
