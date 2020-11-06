44fc75ea01f166839363d5d6ac0a9fb31c7c086d
Agustin <agustinf88@gmail.com>
2020-11-06 15:22:37 -0300

    add M case

--- src/usecases/number/BaseNumberSubstitutorFactory.ts
+++ src/usecases/number/BaseNumberSubstitutorFactory.ts
@@ -1,7 +1,11 @@
 import { NumberSubstitutor, NumberSubstitutorFactory } from "../interfaces";
+import { BaseMNumberSubstitutor } from "./BaseMNumberSubstitutor";
 
-export class BaseNumberSubstitutorFactory implements NumberSubstitutorFactory{
+export class BaseNumberSubstitutorFactory implements NumberSubstitutorFactory {
     create(value: string): NumberSubstitutor {
+        if (value === "M") {
+            return new BaseMNumberSubstitutor();
+        }
         throw new Error("Method not implemented.");
     }
 }
--- test/usecases/number/BaseNumberSubstitutorFactory.test.ts
+++ test/usecases/number/BaseNumberSubstitutorFactory.test.ts
@@ -1,6 +1,14 @@
 import { BaseNumberSubstitutorFactory } from "../../../src/usecases/number/BaseNumberSubstitutorFactory"
+import {BaseMNumberSubstitutor} from "../../../src/usecases/number/BaseMNumberSubstitutor"
 
 test("When unknown value the throws with description", () => {
     const useCase = new BaseNumberSubstitutorFactory();
     expect(() => useCase.create("A")).toThrowError(new Error("Method not implemented."));
+});
+
+test("When M should return instance of BaseNumberSubstitutor", () => {
+    const useCase = new BaseNumberSubstitutorFactory();
+    const numberSubstitutor = useCase.create("M");
+
+    expect(numberSubstitutor).toBeInstanceOf(BaseMNumberSubstitutor)
 });
