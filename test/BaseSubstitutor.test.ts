289fbe07fa94cf192af1ec468b9ef4521625ad23
Agustin <agustinf88@gmail.com>
2020-11-06 00:14:38 -0300

    add throws test

--- test/BaseSubstitutor.test.ts
+++ test/BaseSubstitutor.test.ts
@@ -24,14 +24,27 @@ test('Call BoolSustitutor NumberStrategy and NumberSubstitutor with correct para
     const strategy: NumberStrategy = {
         getNumberSubstitutor: (v: string) => {
             expect(v).toBe("M");
-            return numberUseCase}
+            return numberUseCase
+        }
     }
 
     const useCase = new BaseSubstitutor(boolUseCase, strategy)
-    
-    
+
+
     const actual = useCase.apply(a, b)
     expect(actual.h).toBe("M");
     expect(actual.k).toBe(1);
+
+});
+
+test('When BoolSubstitutor Throws then it should Throws same error', () => {
+    const boolSubstitutor: BoolSubstitutor = {
+        apply: (v: BoolValues) => { throw new Error("Bool combination not implemented"); }
+    }
+
+    const useCase = new BaseSubstitutor(boolSubstitutor, undefined);
+
+    expect(() => useCase.apply({ a: true, b: true, c: true }, undefined))
+        .toThrowError(new Error("Bool combination not implemented"));
 
 })
